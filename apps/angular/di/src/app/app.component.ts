9bdae83307d8515c5489d187a57278dcfd951377
Evgeny Arshinov <earshinov@gmail.com>
2023-12-07 13:00:06 +0100

    feat(exercise:16): angular-di ("Master Dependancy Injection")

--- apps/angular/di/src/app/app.component.ts
+++ apps/angular/di/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { TableComponent } from '@angular-challenges/angular/di';
 import { AsyncPipe, NgFor } from '@angular/common';
-import { Component, Directive } from '@angular/core';
+import { Component, Directive, Input, OnChanges, Self } from '@angular/core';
 import { CurrencyPipe } from './currency.pipe';
-import { CurrencyService } from './currency.service';
+import { Currency, CurrencyService } from './currency.service';
 import { Product, products } from './product.model';
 
 interface ProductContext {
@@ -22,9 +22,32 @@ export class ProductDirective {
   }
 }
 
+@Directive({
+  selector: '[provideCurrency]',
+  standalone: true,
+  providers: [CurrencyService],
+})
+export class ProvideCurrencyDirective implements OnChanges {
+  @Input({ alias: 'provideCurrency', required: true })
+  currencyCode!: Currency['code'];
+
+  constructor(@Self() private currencyService: CurrencyService) {}
+
+  ngOnChanges() {
+    this.currencyService.setState({ code: this.currencyCode });
+  }
+}
+
 @Component({
   standalone: true,
-  imports: [TableComponent, CurrencyPipe, AsyncPipe, NgFor, ProductDirective],
+  imports: [
+    TableComponent,
+    CurrencyPipe,
+    AsyncPipe,
+    NgFor,
+    ProductDirective,
+    ProvideCurrencyDirective,
+  ],
   providers: [CurrencyService],
   selector: 'app-root',
   template: `
@@ -37,7 +60,7 @@ export class ProductDirective {
         </tr>
       </ng-template>
       <ng-template #body product let-product>
-        <tr>
+        <tr [provideCurrency]="product.currencyCode">
           <td>{{ product.name }}</td>
           <td>{{ product.priceA | currency | async }}</td>
           <td>{{ product.priceB | currency | async }}</td>
