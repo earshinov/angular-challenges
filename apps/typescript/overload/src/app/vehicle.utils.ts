512f1cae8d28266c268a4565f4b5b0b9c0ae00f8
Evgeny Arshinov <earshinov@gmail.com>
2023-12-17 12:10:01 +0100

    feat(exercise:15): typescript-overload ("Function Overload") using mapped types

--- apps/typescript/overload/src/app/vehicle.utils.ts
+++ apps/typescript/overload/src/app/vehicle.utils.ts
@@ -1,4 +1,3 @@
-type VehicleType = 'bus' | 'car' | 'moto' | 'bicycle' | 'boat';
 type Fuel = 'diesel' | 'petrol' | 'electric';
 
 interface Bicycle {
@@ -26,30 +25,54 @@ interface Boat {
   type: 'boat';
 }
 
-type Vehicle = Bicycle | Car | Moto | Bus | Boat;
+type VehicleType = 'bus' | 'car' | 'moto' | 'bicycle' | 'boat';
 
-export function createVehicle(
-  type: VehicleType,
+type Vehicles<K extends VehicleType> =
+  // prettier-ignore
+  K extends 'bus' ? Bus :
+  K extends 'car' ? Car :
+  K extends 'moto' ? Moto :
+  K extends 'bicycle' ? Bicycle :
+  K extends 'boat' ? Boat :
+  never;
+
+type Vehicle = Vehicles<VehicleType>;
+
+export function createVehicle<K extends VehicleType & string>(
+  type: K,
+  fuel?: Fuel,
+  capacity?: number,
+  isPublicTransport?: boolean,
+): Vehicles<K>;
+export function createVehicle<K extends VehicleType & string>(
+  type: K,
   fuel?: Fuel,
   capacity?: number,
   isPublicTransport?: boolean,
 ): Vehicle {
   switch (type) {
     case 'bicycle':
-      return { type };
+      return { type } satisfies Bicycle;
     case 'car':
+      if (!fuel) throw new Error(`fuel property is missing for type ${type}`);
+      return { fuel, type } satisfies Car;
     case 'moto':
       if (!fuel) throw new Error(`fuel property is missing for type ${type}`);
-      return { fuel, type };
+      return { fuel, type } satisfies Moto;
     case 'boat':
       if (!capacity)
         throw new Error(`capacity property is missing for type boat`);
-      return { capacity, type };
+      return { capacity, type } satisfies Boat;
     case 'bus':
       if (!capacity)
         throw new Error(`capacity property is missing for type bus`);
       if (!isPublicTransport)
         throw new Error(`isPublicTransport property is missing for type bus`);
-      return { capacity, isPublicTransport, type };
+      return { capacity, isPublicTransport, type } satisfies Bus;
+    default: {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const _typecheck: never = type;
+      throw new Error(`Unknown vehicle type: ${type}`);
+    }
   }
 }
