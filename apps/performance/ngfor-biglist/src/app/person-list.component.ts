62191175b59a6d8ea986013474d92a01f3aab71a
Evgeny Arshinov <earshinov@gmail.com>
2023-12-20 16:07:29 +0100

    feat(exercise:37): performance-ngfor-biglist ("Optimize Big List")

--- apps/performance/ngfor-biglist/src/app/person-list.component.ts
+++ apps/performance/ngfor-biglist/src/app/person-list.component.ts
@@ -1,23 +1,34 @@
+import {
+  CdkFixedSizeVirtualScroll,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+} from '@angular/cdk/scrolling';
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
-import { NgForTrackByModule } from '@angular-challenges/shared/directives';
-import { CommonModule } from '@angular/common';
 import { Person } from './person.model';
 
 @Component({
   selector: 'app-person-list',
   standalone: true,
-  imports: [CommonModule, NgForTrackByModule],
+  imports: [
+    CommonModule,
+    CdkVirtualScrollViewport,
+    CdkFixedSizeVirtualScroll,
+    CdkVirtualForOf,
+  ],
   template: `
     <div class="relative h-[300px] overflow-hidden">
-      <div class="absolute inset-0 overflow-scroll">
+      <cdk-virtual-scroll-viewport
+        itemSize="36"
+        class="absolute inset-0 overflow-scroll">
         <div
-          *ngFor="let person of persons; trackByProp: 'email'"
+          *cdkVirtualFor="let person of persons; trackBy: personTrackBy"
           class="flex h-9 items-center justify-between border-b">
           <h3>{{ person.name }}</h3>
           <p>{{ person.email }}</p>
         </div>
-      </div>
+      </cdk-virtual-scroll-viewport>
     </div>
   `,
   host: {
@@ -27,4 +38,8 @@ import { Person } from './person.model';
 })
 export class PersonListComponent {
   @Input() persons: Person[] = [];
+
+  personTrackBy(_index: number, person: Person) {
+    return person.email;
+  }
 }
