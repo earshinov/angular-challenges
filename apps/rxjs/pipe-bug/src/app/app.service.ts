a301e553258db44778b07bf9929a48a1601fed7c
Evgeny Arshinov <earshinov@gmail.com>
2024-01-10 14:17:35 +0100

    feat(exercise:11): rxjs-pipe-bug ("High Order Operator Bug")

--- apps/rxjs/pipe-bug/src/app/app.service.ts
+++ apps/rxjs/pipe-bug/src/app/app.service.ts
@@ -1,5 +1,13 @@
 import { inject, Injectable } from '@angular/core';
-import { merge, mergeMap, Observable, of, take } from 'rxjs';
+import {
+  catchError,
+  forkJoin,
+  map,
+  Observable,
+  of,
+  switchMap,
+  take,
+} from 'rxjs';
 import { LocalDBService, TopicType } from './localDB.service';
 
 @Injectable({ providedIn: 'root' })
@@ -11,12 +19,13 @@ export class AppService {
   deleteOldTopics(type: TopicType): Observable<boolean> {
     return this.dbService.searchByType(type).pipe(
       take(1),
-      mergeMap((topicToDelete) =>
-        topicToDelete.length > 0
-          ? topicToDelete
-              .map((t) => this.dbService.deleteOneTopic(t.id))
-              .reduce((acc, curr) => merge(acc, curr), of(true))
-          : of(true),
+      switchMap((topicToDelete) =>
+        forkJoin(
+          topicToDelete.map((t) => this.dbService.deleteOneTopic(t.id)),
+        ).pipe(
+          catchError(() => of([false])),
+          map((results) => results.every(Boolean)),
+        ),
       ),
     );
   }
