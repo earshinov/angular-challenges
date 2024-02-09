c20cf629efc0e04b61b59d5b9827d31ab340c630
Evgeny Arshinov <earshinov@gmail.com>
2024-02-09 16:45:12 +0100

    feat(exercise:3): angular-ngfor-enhancement ("Directive Enhancement")
    For a change, an interesting task

--- apps/angular/ngfor-enhancement/src/app/app.component.ts
+++ apps/angular/ngfor-enhancement/src/app/app.component.ts
@@ -1,25 +1,82 @@
 import { NgFor, NgIf } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Directive,
+  EmbeddedViewRef,
+  Input,
+  OnChanges,
+  TemplateRef,
+  ViewContainerRef,
+  inject,
+} from '@angular/core';
 
 interface Person {
   name: string;
 }
 
+@Directive({
+  // eslint-disable-next-line @angular-eslint/directive-selector
+  selector: '[ngForOf][ngForEmpty]',
+  standalone: true,
+})
+export class NgForEmptyDirective<T> implements OnChanges {
+  @Input({ required: true }) ngForEmpty!: TemplateRef<void>;
+  @Input({ required: true }) ngForOf!: T[];
+
+  private viewContainer = inject(ViewContainerRef);
+
+  private renderedView: EmbeddedViewRef<void> | undefined;
+
+  ngOnChanges() {
+    if (this.ngForOf.length == 0 && !this.renderedView)
+      this.renderedView = this.viewContainer.createEmbeddedView(
+        this.ngForEmpty,
+      );
+    else if (this.ngForOf.length > 0 && this.renderedView) {
+      this.renderedView.destroy();
+      this.renderedView = undefined;
+    }
+  }
+}
+
 @Component({
   standalone: true,
-  imports: [NgFor, NgIf],
+  imports: [NgFor, NgIf, NgForEmptyDirective],
   selector: 'app-root',
   template: `
-    <ng-container *ngIf="persons.length > 0; else emptyList">
-      <div *ngFor="let person of persons">
-        {{ person.name }}
-      </div>
-    </ng-container>
+    <div *ngFor="let person of persons; empty: emptyList">
+      {{ person.name }}
+    </div>
     <ng-template #emptyList>The list is empty !!</ng-template>
+    <p ngPreserveWhitespaces>
+      <button (click)="addPerson($event)">Add person</button>
+      <button (click)="removePerson($event)">Remove person</button>
+    </p>
   `,
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  persons: Person[] = [];
+  protected persons: Person[] = [];
+
+  protected addPerson(event: MouseEvent) {
+    if (event.defaultPrevented) return;
+    event.preventDefault();
+
+    this.persons = [
+      ...this.persons,
+      {
+        name: 'John Doe',
+      },
+    ];
+  }
+
+  protected removePerson(event: MouseEvent) {
+    if (event.defaultPrevented) return;
+    event.preventDefault();
+
+    if (this.persons.length)
+      this.persons = this.persons.slice(0, this.persons.length - 1);
+  }
 }
