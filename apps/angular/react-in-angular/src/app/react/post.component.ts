7208419ac1996c5784034b6375e0b3a001554f76
Evgeny Arshinov <earshinov@gmail.com>
2024-02-25 19:09:39 +0100

    feat(exercise:45): angular-react-in-angular ("React in angular")

--- apps/angular/react-in-angular/src/app/react/post.component.ts
+++ apps/angular/react-in-angular/src/app/react/post.component.ts
@@ -1,4 +1,17 @@
-import { Component, EventEmitter, input, Output } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  input,
+  OnChanges,
+  OnDestroy,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { createRoot, Root } from 'react-dom/client';
+
+import ReactPost from './ReactPost';
 
 type Post = { title: string; description: string; pictureLink: string };
 
@@ -6,12 +19,45 @@ type Post = { title: string; description: string; pictureLink: string };
   standalone: true,
   selector: 'app-post',
   template: `
-    <div></div>
+    <div #root></div>
   `,
   styles: [''],
 })
-export class PostComponent {
+export class PostComponent implements AfterViewInit, OnChanges, OnDestroy {
   post = input<Post | undefined>(undefined);
   isSelected = input<boolean>(false);
   @Output() selectPost = new EventEmitter<void>();
+
+  @ViewChild('root') private root: ElementRef<HTMLDivElement> | undefined;
+  private reactRoot: Root | undefined;
+
+  ngAfterViewInit() {
+    if (this.root) {
+      this.reactRoot = createRoot(this.root.nativeElement);
+      this.update();
+    }
+  }
+
+  ngOnChanges() {
+    this.update();
+  }
+
+  private update() {
+    const post = this.post();
+    this.reactRoot?.render(
+      ReactPost({
+        title: post?.title,
+        description: post?.description,
+        pictureLink: post?.pictureLink,
+        selected: this.isSelected(),
+        handleClick: () => {
+          this.selectPost.emit();
+        },
+      }),
+    );
+  }
+
+  ngOnDestroy() {
+    this.reactRoot?.unmount();
+  }
 }
