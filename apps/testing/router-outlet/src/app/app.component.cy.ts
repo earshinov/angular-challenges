636c79872279aecef1b9154c6f99b24a27191f16
Evgeny Arshinov <earshinov@gmail.com>
2024-03-03 22:59:23 +0100

    feat(exercise:17): testing-router ("Router") with Testing Library and Cypress component tests

--- apps/testing/router-outlet/src/app/app.component.cy.ts
+++ apps/testing/router-outlet/src/app/app.component.cy.ts
@@ -1,33 +1,76 @@
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
 
-describe(AppComponent.name, () => {
+const selectors = {
+  borrowButton() {
+    return cy.get('button').contains('Borrow');
+  },
+  searchInput() {
+    return cy
+      .get('label')
+      .contains(/Search book/i)
+      .find('input');
+  },
+};
+
+const actions = {
+  async searchBy(text: string) {
+    selectors.searchInput().focus().type(text);
+    selectors.borrowButton().click();
+  },
+  async checkEntries(expectedEntries: RegExp[]) {
+    cy.find('li')
+      .should('have.length', expectedEntries.length)
+      .each((actual, i) => {
+        expect(actual.text()).to.match(expectedEntries[i]);
+      });
+  },
+};
+
+beforeEach(() => {
+  cy.mount(AppComponent, {
+    providers: [provideRouter(appRoutes)],
+  });
+});
+
+describe('AppComponent', () => {
   describe('Given no search criteria', () => {
-    it('Then shows error message and disabled button', () => {
-      //todo
+    it('Then shows error message and disabled button', async () => {
+      cy.find('div')
+        .contains(/Search criteria is required!/)
+        .should('not.be.null');
+      selectors.borrowButton().should('not.be.disabled');
     });
   });
 
   describe('Given a search criteria with no book match', () => {
-    it('Then shows No book found', () => {
-      //todo
+    it('Then shows No book found', async () => {
+      actions.searchBy('Crap');
+      cy.find('div')
+        .contains(/No book found/)
+        .should('not.be.null');
     });
   });
 
   describe('Given a search criteria with one book match', () => {
-    it('Then shows One book and no error', () => {
-      //todo
+    it('Then shows One book and no error', async () => {
+      actions.searchBy('Mockingbird');
+      actions.checkEntries([/To Kill a Mockingbird/]);
     });
   });
 
   describe('Given a search criteria in Uppercase with one book match', () => {
-    it('Then shows One book and no error', () => {
-      //todo
+    it('Then shows One book and no error', async () => {
+      actions.searchBy('MOCKINGBIRD');
+      actions.checkEntries([/To Kill a Mockingbird/]);
     });
   });
 
   describe('Given a search criteria with multple books matches', () => {
-    it('Then shows a list of books', () => {
-      //todo
+    it('Then shows a list of books', async () => {
+      actions.searchBy('Tolkien');
+      actions.checkEntries([/The Hobbit/, /The Lord of the Rings/]);
     });
   });
 });
--- apps/testing/router-outlet/src/app/app.component.spec.ts
+++ apps/testing/router-outlet/src/app/app.component.spec.ts
@@ -1,31 +1,93 @@
+import { RenderResult, render } from '@testing-library/angular';
+import userEvent, { UserEvent } from '@testing-library/user-event';
+import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
+
+async function setup() {
+  const app = await render(AppComponent, { routes: appRoutes });
+  const user = userEvent.setup();
+  return { app, user };
+}
+
+type App = RenderResult<AppComponent, AppComponent>;
+
+function selectors(app: App) {
+  return {
+    async borrowButton() {
+      return (await app.findByRole('button', {
+        name: 'Borrow',
+      })) as HTMLButtonElement;
+    },
+    async searchInput() {
+      return (await app.findByLabelText(/Search book/i)) as HTMLInputElement;
+    },
+  };
+}
+
+function actions(app: App, user: UserEvent) {
+  return {
+    async searchBy(text: string) {
+      const sel = selectors(app);
+      (await sel.searchInput()).focus();
+      await user.keyboard(text);
+      await user.click(await sel.borrowButton());
+    },
+    async checkEntries(expectedEntries: (string | RegExp)[]) {
+      const actualEntries = await app.findAllByText(/Borrowed book/i);
+      expect(actualEntries).toHaveLength(expectedEntries.length);
+      actualEntries.forEach((actual, i) => {
+        const expected = expectedEntries[i];
+        expect(actual!).toHaveTextContent(expected!);
+      });
+    },
+  };
+}
+
 describe('AppComponent', () => {
   describe('Given no search criteria', () => {
     it('Then shows error message and disabled button', async () => {
-      //todo
+      const { app } = await setup();
+
+      expect(
+        await app.findByText(/Search criteria is required!/),
+      ).toBeInTheDocument();
+
+      expect(await selectors(app).borrowButton()).toBeDisabled();
     });
   });
 
   describe('Given a search criteria with no book match', () => {
     it('Then shows No book found', async () => {
-      //todo
+      const { app, user } = await setup();
+      await actions(app, user).searchBy('Crap');
+      expect(await app.findByText(/No book found/)).toBeInTheDocument();
     });
   });
 
   describe('Given a search criteria with one book match', () => {
     it('Then shows One book and no error', async () => {
-      //todo
+      const { app, user } = await setup();
+      await actions(app, user).searchBy('Mockingbird');
+      await actions(app, user).checkEntries([/To Kill a Mockingbird/]);
     });
   });
 
   describe('Given a search criteria in Uppercase with one book match', () => {
     it('Then shows One book and no error', async () => {
-      //todo
+      const { app, user } = await setup();
+      await actions(app, user).searchBy('MOCKINGBIRD');
+      await actions(app, user).checkEntries([/To Kill a Mockingbird/]);
     });
   });
 
   describe('Given a search criteria with multple books matches', () => {
     it('Then shows a list of books', async () => {
-      //todo
+      const { app, user } = await setup();
+      await actions(app, user).searchBy('Tolkien');
+      await actions(app, user).checkEntries([
+        /The Hobbit/,
+        /The Lord of the Rings/,
+      ]);
     });
   });
 });
