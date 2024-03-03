5aa4b73f38641a7ae187ad1488a179633b237fb2
Evgeny Arshinov <earshinov@gmail.com>
2024-03-03 21:41:44 +0100

    feat(exercise:28): testing-checkbox ("Checkbox")

--- apps/testing/checkbox/src/app/app.component.spec.ts
+++ apps/testing/checkbox/src/app/app.component.spec.ts
@@ -1,10 +1,23 @@
 import { render } from '@testing-library/angular';
+import { logRoles, screen } from '@testing-library/dom';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   describe('When checking the checkbox', () => {
     it('Then button is enabled', async () => {
-      await render(AppComponent);
+      const el = await render(AppComponent);
+
+      console.log('Playground URL:');
+      screen.logTestingPlaygroundURL();
+
+      console.log('ARIA roles:');
+      logRoles(el.debugElement.nativeElement);
+
+      console.log('DOM:');
+      screen.debug();
+
+      console.log('Element DOM:');
+      screen.debug(el.debugElement.nativeElement);
     });
   });
 });
