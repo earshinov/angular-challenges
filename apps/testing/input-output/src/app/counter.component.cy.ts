7741e6d9973e69fbeac5ae27ba91eaa1775c54bc
Evgeny Arshinov <earshinov@gmail.com>
2024-03-09 22:45:36 +0100

    feat(exercise:19): testing-input-output ("Input Output")

--- apps/testing/input-output/src/app/counter.component.cy.ts
+++ apps/testing/input-output/src/app/counter.component.cy.ts
@@ -1,13 +1,41 @@
+import { createOutputSpy } from 'cypress/angular';
 import { CounterComponent } from './counter.component';
+
+function setup() {
+  cy.mount(CounterComponent, {
+    componentProperties: {
+      initialValue: 10,
+      send: createOutputSpy('send'),
+    },
+    // or `autoSpyOutputs: true` and use '@sendSpy' instead of '@send' below
+  });
+}
 
 describe(CounterComponent.name, () => {
   describe('Given an initualValue of 10', () => {
-    it('listen to output using createOutputSpy', () => {
-      cy.mount(CounterComponent);
+    test('Then counterValue is 10', async () => {
+      setup();
+      cy.findByText(/Counter: /i).should('have.text', /Counter: 10/i);
     });
+  });
 
-    it('listen to output  using autoSpyOutputs', () => {
-      cy.mount(CounterComponent);
+  describe('When clicking 5 times on increment button', () => {
+    test('Then counterValue is 15', async () => {
+      setup();
+      for (const _ of new Array(5))
+        cy.findByRole('button', { name: 'Increment' }).click();
+      cy.findByText(/Counter: /i).should('have.text', /Counter: 15/i);
+    });
+  });
+
+  describe('When clicking 2 times on decrement button', () => {
+    test('Then counterValue is 8 and emitted value is 8', async () => {
+      setup();
+      for (const _ of new Array(2))
+        cy.findByRole('button', { name: 'Decrement' }).click();
+      cy.findByText(/Counter: /i).should('have.text', /Counter: 8/i);
+      cy.findByRole('button', { name: 'Send' }).click();
+      cy.get('@send').should('have.been.calledOnceWith', 8);
     });
   });
 });
--- apps/testing/input-output/src/app/counter.component.spec.ts
+++ apps/testing/input-output/src/app/counter.component.spec.ts
@@ -1,27 +1,47 @@
-import { render } from '@testing-library/angular';
+import { render, screen } from '@testing-library/angular';
+import { userEvent as user } from '@testing-library/user-event';
 import { CounterComponent } from './counter.component';
+
+async function renderDefault() {
+  const result = await render(CounterComponent, {
+    componentInputs: {
+      initialValue: 10,
+    },
+  });
+  return { ...result, component: result.fixture.componentInstance };
+}
 
 describe('CounterComponent', () => {
   describe('Given an initualValue of 10', () => {
     test('Then counterValue is 10', async () => {
-      await render(CounterComponent);
+      await renderDefault();
+      expect(screen.getByText(/Counter: /i)).toHaveTextContent(/Counter: 10/i);
     });
+  });
 
-    describe('When clicking 5 times on increment button', () => {
-      test('Then counterValue is 15', async () => {
-        await render(CounterComponent);
-      });
+  describe('When clicking 5 times on increment button', () => {
+    test('Then counterValue is 15', async () => {
+      await renderDefault();
+      for (const _ of new Array(5))
+        await user.click(screen.getByRole('button', { name: 'Increment' }));
+      expect(screen.getByText(/Counter: /i)).toHaveTextContent(/Counter: 15/i);
     });
+  });
 
-    describe('When clicking 2 times on decrement button', () => {
-      test('Then counterValue is 8', async () => {
-        await render(CounterComponent);
-      });
-      describe('When clicking on Send ', () => {
-        test('Then emitted value is 8', async () => {
-          await render(CounterComponent);
-        });
-      });
+  describe('When clicking 2 times on decrement button', () => {
+    test('Then counterValue is 8 and emitted value is 8', async () => {
+      const { component } = await renderDefault();
+      for (const _ of new Array(2))
+        await user.click(screen.getByRole('button', { name: 'Decrement' }));
+      expect(screen.getByText(/Counter: /i)).toHaveTextContent(/Counter: 8/i);
+
+      const values: number[] = [];
+      component.send.subscribe((value) => values.push(value));
+      await new Promise((resolve) => window.setTimeout(resolve));
+      expect(values).toEqual([]);
+
+      await user.click(screen.getByRole('button', { name: 'Send' }));
+      expect(values).toEqual([8]);
     });
   });
 });
