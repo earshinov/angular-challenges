969c5437476ddee31d54e5f209c83e75b764deeb
Evgeny Arshinov <earshinov@gmail.com>
2024-03-09 22:15:18 +0100

    feat(exercise:18): testing-nested ("Nested Components")

--- apps/testing/nested/src/app/child.component.cy.ts
+++ apps/testing/nested/src/app/child.component.cy.ts
@@ -1,18 +1,30 @@
+import { TestBed } from '@angular/core/testing';
 import { ChildComponent } from './child.component';
+import { HttpService } from './http.service';
 
 describe('ChildComponent', () => {
   const setup = () => {
-    cy.mount(ChildComponent);
+    cy.mount(ChildComponent).then(() => {
+      const http = TestBed.inject(HttpService);
+      cy.stub(http, 'sendTitle').as('http');
+    });
   };
   describe('When typing nothing and clicking on Validate', () => {
     test('Then show "Title is required" error message and no http request has been sent', async () => {
       setup();
+      cy.findByRole('button', { name: 'Validate' }).click();
+      cy.findByText(/Title is required/i).should('exist');
+      cy.get('@http').should('not.be.called');
     });
   });
-
   describe('When typing "Good" and clicking on Validate', () => {
     test('Then show "Title is Good" message, no error message and send a http request to the backend', async () => {
       setup();
+      cy.findByRole('textbox').type('Good');
+      cy.findByText(/Title is Good/i).should('contain.text', 'Good');
+      cy.findByRole('button', { name: 'Validate' }).click();
+      cy.findByText(/Title is required/i).should('not.exist');
+      cy.get('@http').should('be.called');
     });
   });
 });
--- apps/testing/nested/src/app/child.component.spec.ts
+++ apps/testing/nested/src/app/child.component.spec.ts
@@ -1,16 +1,44 @@
-import { render } from '@testing-library/angular';
+import { render, screen } from '@testing-library/angular';
+import { userEvent as user } from '@testing-library/user-event';
 import { ChildComponent } from './child.component';
+import { HttpService } from './http.service';
 
 describe('ChildComponent', () => {
   describe('When typing nothing and clicking on Validate', () => {
     test('Then show "Title is required" error message and no http request has been sent', async () => {
-      await render(ChildComponent);
+      const { http } = await renderChildComponent();
+      await user.click(screen.getByRole('button', { name: 'Validate' }));
+      expect(screen.getByText(/Title is required/i)).toBeInTheDocument();
+      expect(http.sendTitle).not.toBeCalled();
     });
   });
 
   describe('When typing "Good" and clicking on Validate', () => {
     test('Then show "Title is Good" message, no error message and send a http request to the backend', async () => {
-      await render(ChildComponent);
+      const { http } = await renderChildComponent();
+      await user.type(screen.getByRole('textbox'), 'Good');
+      expect(screen.getByText(/Title is Good/i)).toHaveTextContent(/Good/); // Must exist and preserve case
+      await user.click(screen.getByRole('button', { name: 'Validate' }));
+      expect(screen.queryByText(/Title is required/i)).not.toBeInTheDocument();
+      expect(http.sendTitle).toBeCalledTimes(1);
+      expect(http.sendTitle).toBeCalledWith('Good');
     });
   });
 });
+
+class MockHttpService implements HttpService {
+  sendTitle = jest.fn();
+}
+
+async function renderChildComponent() {
+  const http = new MockHttpService();
+  const renderResult = await render(ChildComponent, {
+    providers: [
+      {
+        provide: HttpService,
+        useValue: http,
+      },
+    ],
+  });
+  return { ...renderResult, http };
+}
